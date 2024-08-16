a3ad9c0bba94b7429748bac12ac3a5e3129e82b2
shubhamhazard <shubham555bgp@gmail.com>
2024-08-16 14:12:46 +0530

    new

---
+++ chai.js
@@ -0,0 +1,11 @@
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname); //nesting
